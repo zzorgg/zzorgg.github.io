52f490d2ede7ed551db5ef15fe9e663cb82a62a4
saifk16 <saif.khan16@outlook.com>
2025-09-15 01:52:09 +0530

    added workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  // If your repository name is not the same as your GitHub username,
+  // uncomment and update the basePath below:
+  // basePath: '/your-repository-name',
 };
 
 export default nextConfig;
